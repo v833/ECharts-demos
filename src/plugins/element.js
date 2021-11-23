90f50f4710d8e2f4c4c74f386637bc26b4d33f34
v833 <2507301541@qq.com>
2021-11-23 17:03:28 +0800

    销售额组件开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import {
-  Card, Row, Col
+  Card, Row, Col, Menu, MenuItem,RadioButton, RadioGroup, Radio, DatePicker
 } from 'element-ui'
 
 Vue.use(Card)
 Vue.use(Row)
 Vue.use(Col)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(RadioGroup)
+Vue.use(Radio)
+Vue.use(DatePicker)
+Vue.use(RadioButton)
